e404cedb9bbdd9214bcb571cc95b2f53f0f3986a
oscar <oscaroceguera@gmail.com>
2016-04-03 00:01:46 -0700

    refactor API with underscore

--- server.js
+++ server.js
@@ -1,24 +1,12 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
+var _ = require('underscore')
 var PORT = process.env.PORT || 3000
+var todos = []
+var todoNextId = 1
 
-var todos = [
-	{
-		id: 1,
-		description: "meet mom for lunch",
-		completed: false
-	},
-	{
-		id: 2,
-		description: "Go to market",
-		completed: false
-	},
-	{
-		id: 3,
-		description: "Go to party",
-		completed: true
-	}
-]
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
 	res.send('Todo API Root')
@@ -29,21 +17,31 @@ app.get('/todos', function(req, res){
 })
 
 app.get('/todos/:id', function(req, res){
-	var id = parseInt(req.params.id);
-	var matchedTodo;
-
-	todos.forEach(function(todo){
-		if (todo.id === id) {
-			matchedTodo = todo
-		}
-	});
+	var todoId = parseInt(req.params.id);
+	var matchedTodo = _.findWhere(todos, {id: todoId})
 
 	if(matchedTodo){
 		res.json(matchedTodo)
 	} else{
 		res.status(404).send()
 	}
+})
 
+app.post('/todos', function(req, res){
+	var body = _.pick(req.body, 'description', 'completed');
+
+
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send()
+	}
+
+	body.description = body.description.trim()
+	body.id = todoNextId++
+
+	todos.push(body)
+
+	res.json(body)
 })
 
 app.listen(PORT, function(){
