311e682120ff42fc1929a183d219a4f6007e2e81
oscar <oscaroceguera@gmail.com>
2016-04-04 22:16:18 -0600

    Add token generation to POST /users

--- server.js
+++ server.js
@@ -128,13 +128,21 @@ app.post('/users/login', function(req, res){
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.authenticate(body).then(function(user){
-		res.json(user.toPublicJSON())
-	}, function(e){
+		var token = user.generateToken('authentication');
+
+		if (token) {
+			res.header('Auth', token).json(user.toPublicJSON())
+		} else {
+			res.status(401).send()
+		}
+
+	}, function(){
 		res.status(401).send()
 	})
 
 })
 
+// {force: true} - delete the database
 db.sequelize.sync({force: true}).then(function(){
 	app.listen(PORT, function(){
 		console.log('Done ' + PORT);
