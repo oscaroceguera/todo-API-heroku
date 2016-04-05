82bee67d63d09d32998754f001cdb0b65b6346f8
oscar <oscaroceguera@gmail.com>
2016-04-05 00:41:31 -0600

    Comvert CRUD reques to use current user todos only

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.get('/', function(req, res){
 // GET /todos?completed=fase&q=work
 app.get('/todos', middleware.requireAuthentication, function(req, res){
 	var query = req.query
-	var where = {}
+	var where = {
+		userId: req.user.get('id')
+	};
 
 	if (query.hasOwnProperty('completed') && query.completed === 'true') {
 		where.completed = true
@@ -43,7 +45,12 @@ app.get('/todos', middleware.requireAuthentication, function(req, res){
 app.get('/todos/:id',middleware.requireAuthentication, function(req, res){
 	var todoId = parseInt(req.params.id);
 
-	db.todo.findById(todoId).then(function(todo){
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo){
 		if (!!todo) {
 			res.json(todo.toJSON())
 		} else {
@@ -73,7 +80,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req,res){
 
 	db.todo.destroy({
 		where: {
-			id: todoId
+			id: todoId,
+			userId: req.user.get('id')
 		}
 	}).then(function(rowsDelete){
 		if (rowsDelete === 0) {
@@ -101,7 +109,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res){
 		attributes.description = body.description
 	}
 
-	db.todo.findById(todoId)
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	})
 	.then(function(todo){
 		if (todo) {
 			todo.update(attributes)
@@ -149,7 +162,7 @@ app.post('/users/login', function(req, res){
 
 // {force: true} - delete the database
 db.sequelize.sync({
-	force: true
+	//force: true
 }).then(function(){
 	app.listen(PORT, function(){
 		console.log('Done ' + PORT);
